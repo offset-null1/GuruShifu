d6aa64c680e711e1d04aaeb615c13fb4db6e44c9
Dakshta Tomar <nirvindak23@gmail.com>
2022-09-03 04:28:07 +0000

    level 3

--- guru-shifu/Rectangle/src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ guru-shifu/Rectangle/src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -8,10 +8,10 @@ import static org.hamcrest.Matchers.is;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
-@DisplayName("Writing assertions for rectangle")
+@DisplayName("Writing assertions for calculating area and perimeter of rectangle")
 public class RectangleTest {
 
-    @DisplayName("When length is zero")
+    @DisplayName("Area when length is zero")
     @Test
     void shouldReturnAreaWhenLengthIsZero() {
         Rectangle rect = new Rectangle(0.0, 4.0);
@@ -20,7 +20,7 @@ public class RectangleTest {
         
     }
 
-    @DisplayName("When breadth is zero")
+    @DisplayName("Area when breadth is zero")
     @Test
     void shouldReturnAreaWhenBreadthIsZero() {
         Rectangle rect = new Rectangle(5.0, 0.0);
@@ -28,7 +28,7 @@ public class RectangleTest {
         assertThat(res, is(closeTo(0.0, 0.001)));
     }
 
-    @DisplayName("When length and breadth is integer")
+    @DisplayName("Area when length and breadth are integer")
     @Test
     void shouldReturnAreaWhenLengthAndBreadthAreGiven() {
         Rectangle rect = new Rectangle(36.34, 21.35);
@@ -36,4 +36,33 @@ public class RectangleTest {
         assertThat(res, is(closeTo(775.859, 0.001)));
         
     }
+
+
+    @DisplayName("Perimeter when length is zero")
+    @Test
+    void shouldReturnPerimeterWhenLengthIsZero(){
+        Rectangle rect = new Rectangle(0.0, 20.0);
+        double res = rect.perimeter();
+        assertThat(res, is(closeTo(40.0, 0.001)));
+
+    }
+
+    @DisplayName("Perimeter when breadth is zero")
+    @Test
+    void shouldReturnPerimeterWhenBreadthIsZero(){
+        Rectangle rect = new Rectangle(20.0, 0.0);
+        double res = rect.perimeter();
+        assertThat(res, is(closeTo(40.0, 0.001)));
+    }
+
+    @DisplayName("Perimeter when length and breath are given")
+    @Test
+    void shouldReturnPerimeterWhenLengthAndBreadthAreGiven(){
+        Rectangle rect = new Rectangle(20.0, 20.0);
+        double res = rect.perimeter();
+        assertThat(res, is(closeTo(80.0, 0.001)));
+    }
+
+
+
 }
